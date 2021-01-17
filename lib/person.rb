c2f80867178d550d6cb0e53f682fd0cfe8636d74
Luis Castillo <raul@Luiss-MacBook-Pro.local>
2021-01-17 16:05:48 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
-  #your code here
+  
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize(avi_attributes)
+     avi_attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
+
 end
+
+
+# Person
+#   a longer list of attributes
+#     will assign an arbitrary number of properties on initialization
+#   a different, shorter list of attributes
+#     will also assign an arbitrary number of properties on initialization
+
+# Finished in 0.00486 seconds (files took 0.17763 seconds to load)
+# 2 examples, 0 failures
